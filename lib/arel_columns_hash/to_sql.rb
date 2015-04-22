37e4da6ffb4e1500acce92003040a66819bf43ca
Genki Sugawara <sugawara@cookpad.com>
2015-04-22 13:40:16 +0900

    fix if engine eq ActiveRecord::Base

--- lib/arel_columns_hash/to_sql.rb
+++ lib/arel_columns_hash/to_sql.rb
@@ -4,12 +4,32 @@ class Arel::Visitors::ToSql
     name     = attr.name.to_s
     relation = attr.relation
 
-    if has_columns_hash? relation
-      relation.engine.columns_hash[name]
+    relation_engine = get_relation_engine_has_columns_hash(relation)
+
+    if relation_engine
+      relation_engine.columns_hash[name]
     elsif table_exists? relation.table_name
       column_cache(relation.table_name)[name]
     else
       nil
+    end
+  end
+
+  def get_relation_engine_has_columns_hash(relation)
+    return nil has_columns_hash?(relation)
+
+    relation_engine = relation.engine
+
+    if relation.engine == ActiveRecord::Base
+      relation_engine = relation.table_name.classify.constantize rescue nil
+
+      if relation_engine.is_a?(Class) && relation_engine < ActiveRecord::Base
+        relation_engine
+      else
+        nil
+      end
+    else
+      relation.engine
     end
   end
 
